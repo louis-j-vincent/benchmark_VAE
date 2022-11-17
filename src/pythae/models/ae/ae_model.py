318d7ff3690e2a5b37673e30f1fd24cd60a83914
Louis VINCENT <louisjvincent@gmail.com>
2022-11-17 13:13:59 +0100

    Updated with changes from SNDS platform

--- src/pythae/models/ae/ae_model.py
+++ src/pythae/models/ae/ae_model.py
@@ -13,6 +13,277 @@ from torch import tensor, cat, exp, std
 import torch
 from numpy.random import binomial
 
+
+class AE_multi_U(BaseAE):
+    """Vanilla Autoencoder model.
+
+    Args:
+        model_config (AEConfig): The Autoencoder configuration setting the main parameters of the
+            model.
+
+        encoder (BaseEncoder): An instance of BaseEncoder (inheriting from `torch.nn.Module` which
+            plays the role of encoder. This argument allows you to use your own neural networks
+            architectures if desired. If None is provided, a simple Multi Layer Preception
+            (https://en.wikipedia.org/wiki/Multilayer_perceptron) is used. Default: None.
+
+        decoder (BaseDecoder): An instance of BaseDecoder (inheriting from `torch.nn.Module` which
+            plays the role of decoder. This argument allows you to use your own neural networks
+            architectures if desired. If None is provided, a simple Multi Layer Preception
+            (https://en.wikipedia.org/wiki/Multilayer_perceptron) is used. Default: None.
+
+    .. note::
+        For high dimensional data we advice you to provide you own network architectures. With the
+        provided MLP you may end up with a ``MemoryError``.
+    """
+
+    def __init__(
+        self,
+        model_config: AEConfig,
+        encoder: Optional[BaseEncoder] = None,
+        decoder: Optional[BaseDecoder] = None,
+    ):
+
+        BaseAE.__init__(self, model_config=model_config, decoder=decoder)
+
+        self.model_name = "AE_multi_U"
+
+        if encoder is None:
+            if model_config.input_dim is None:
+                raise AttributeError(
+                    "No input dimension provided !"
+                    "'input_dim' parameter of BaseAEConfig instance must be set to 'data_shape' where "
+                    "the shape of the data is (C, H, W ..). Unable to build encoder "
+                    "automatically"
+                )
+
+            encoder = Encoder_AE_MLP(model_config)
+            self.model_config.uses_default_encoder = True
+
+        else:
+            self.model_config.uses_default_encoder = False
+
+        self.set_encoder(encoder)
+        self.p = 0 #p for the bernoulli encoding the number of missing values
+        self.nU = 10
+        self.is_merging_net = False
+
+    def add_missing_values(self, x, u, nU):
+        """
+        expands x nU times, adds missing values folllowing a new mask U for each of the nU repeats
+        """
+        x_nU, u_nU = x.repeat((nU,1)), u.repeat((nU,1))
+        #set xU as nU repeat of x with a different U applied each time
+        xU = x_nU.detach().clone()
+        binomial_probas = (u_nU)*self.p #we want to delete each feat with proba self.ps
+        U = tensor(binomial(n=1,p=binomial_probas,size=xU.shape))
+        xU[U==1] = -10
+
+        return xU, x_nU, u_nU
+
+    def forward(self, inputs: BaseDataset, **kwargs) -> ModelOutput:
+        """The input data is encoded and decoded
+
+        Args:
+            inputs (BaseDataset): An instance of pythae's datasets
+
+        Returns:
+            ModelOutput: An instance of ModelOutput containing all the relevant parameters
+            - nU is the number of time we wish to makes copies of x and apply a different U
+            When self.p>0, we repeat nU times x and apply a different u everytime
+
+        """
+
+        x = inputs["data"]
+        u = (x!=-10)
+        nU = self.nU if self.training else 3
+        z = self.encoder(x).embedding 
+
+        if self.p>0:
+            xU, x_repeat, u_repeat = self.add_missing_values(x, u, nU)
+            zU = self.encoder(xU).embedding #encoding of xU
+
+        #if SNDS-like data, get z_alpha the equivalent of zU for SNDS-like data
+        is_x_alpha = (inputs['labels']==1.).float().mean()!=1.
+        if is_x_alpha:
+            x_alpha = inputs["labels"]
+            z_alpha = self.encoder.encoder_alpha(x_alpha)#.embedding
+            z_alpha = z_alpha.repeat((nU,1))
+
+        #Concatenate zU and z_alpha to get information from both for Z
+        if self.is_merging_net:
+            z = cat((zU,z_alpha),axis=1)
+            z = self.encoder.merging_net(z)
+        else:
+            z = zU
+
+        recon_x = self.decoder(z)["reconstruction"]
+        loss = self.loss_function(recon_x[u_repeat], x_repeat[u_repeat])
+        
+        if is_x_alpha:
+            loss += self.loss_function(z_alpha,zU) 
+        else:
+            loss += self.loss_function(z_repeat,zU) # + exp(-self.loss_function(z,z*0.))
+
+        output = ModelOutput(loss=loss, recon_x=recon_x, z=z)
+
+        return output
+
+    def loss_function(self, recon_x, x):
+
+        MSE = F.mse_loss(
+            recon_x.reshape(x.shape[0], -1), x.reshape(x.shape[0], -1), reduction="none"
+        ).sum(dim=-1)
+        return MSE.mean(dim=0)
+
+    @classmethod
+    def _load_model_config_from_folder(cls, dir_path):
+        file_list = os.listdir(dir_path)
+
+        if "model_config.json" not in file_list:
+            raise FileNotFoundError(
+                f"Missing model config file ('model_config.json') in"
+                f"{dir_path}... Cannot perform model building."
+            )
+
+        path_to_model_config = os.path.join(dir_path, "model_config.json")
+        model_config = AEConfig.from_json_file(path_to_model_config)
+
+        return model_config
+
+class vAE(BaseAE): #equivalent of AE_multi_U_w_variance
+    """Vanilla Autoencoder model.
+
+    Args:
+        model_config (AEConfig): The Autoencoder configuration setting the main parameters of the
+            model.
+
+        encoder (BaseEncoder): An instance of BaseEncoder (inheriting from `torch.nn.Module` which
+            plays the role of encoder. This argument allows you to use your own neural networks
+            architectures if desired. If None is provided, a simple Multi Layer Preception
+            (https://en.wikipedia.org/wiki/Multilayer_perceptron) is used. Default: None.
+
+        decoder (BaseDecoder): An instance of BaseDecoder (inheriting from `torch.nn.Module` which
+            plays the role of decoder. This argument allows you to use your own neural networks
+            architectures if desired. If None is provided, a simple Multi Layer Preception
+            (https://en.wikipedia.org/wiki/Multilayer_perceptron) is used. Default: None.
+
+    .. note::
+        For high dimensional data we advice you to provide you own network architectures. With the
+        provided MLP you may end up with a ``MemoryError``.
+    """
+
+    def __init__(
+        self,
+        model_config: AEConfig,
+        encoder: Optional[BaseEncoder] = None,
+        decoder: Optional[BaseDecoder] = None,
+    ):
+
+        BaseAE.__init__(self, model_config=model_config, decoder=decoder)
+
+        self.model_name = "AE_multi_U"
+
+        if encoder is None:
+            if model_config.input_dim is None:
+                raise AttributeError(
+                    "No input dimension provided !"
+                    "'input_dim' parameter of BaseAEConfig instance must be set to 'data_shape' where "
+                    "the shape of the data is (C, H, W ..). Unable to build encoder "
+                    "automatically"
+                )
+
+            encoder = Encoder_AE_MLP(model_config)
+            self.model_config.uses_default_encoder = True
+
+        else:
+            self.model_config.uses_default_encoder = False
+
+        self.set_encoder(encoder)
+        self.p = 0.1 #p for the bernoulli encoding the number of missing values
+        self.nU = 10
+        self.init_loss = True
+
+    def add_missing_values(self, x, u, nU):
+        """
+        expands x nU times, adds missing values folllowing a new mask U for each of the nU repeats
+        """
+        x_nU, u_nU = x.repeat((nU,1)), u.repeat((nU,1))
+        #set xU as nU repeat of x with a different U applied each time
+        xU = x_nU.detach().clone()
+        binomial_probas = (u_nU)*self.p #we want to delete each feat with proba self.ps
+        U = tensor(binomial(n=1,p=binomial_probas,size=xU.shape))
+        xU[U==1] = -10
+
+        return xU, x_nU, u_nU
+
+    def forward(self, inputs: BaseDataset, **kwargs) -> ModelOutput:
+        """The input data is encoded and decoded
+
+        Args:
+            inputs (BaseDataset): An instance of pythae's datasets
+
+        Returns:
+            ModelOutput: An instance of ModelOutput containing all the relevant parameters
+            - nU is the number of time we wish to makes copies of x and apply a different U
+            When self.p>0, we repeat nU times x and apply a different u everytime
+
+        """
+
+        x = inputs["data"]
+        u = (x!=-10)
+        nU = self.nU if self.training else 3
+        z = self.encoder(x).embedding 
+
+        if self.p>0:
+            xU, x_repeat, u_repeat = self.add_missing_values(x, u, nU)
+            z_out = self.encoder(xU) #encoding of xU
+            zU_mu, zU_sigma = z_out.embedding, z_out.log_covariance
+
+        z_anchor = z.repeat((nU,1))
+        recon_x = self.decoder(zU_mu)["reconstruction"]
+
+        recon_loss = self.loss_function(recon_x[u_repeat], x_repeat[u_repeat])
+        if self.init_loss:
+            reg_loss = self.loss_function(z_anchor, zU_mu)
+        else:
+            reg_loss = self.loss_log_proba(z_anchor, zU_mu, zU_sigma)
+
+        loss = recon_loss + 0.1*reg_loss
+
+        output = ModelOutput(loss=loss, recon_x=recon_x, z=z)
+
+        return output
+
+    def loss_log_proba(self, x, mu, sigma):
+        sigma = torch.abs(sigma)
+        eps = torch.tensor(1e-5)
+        loss_mu = ( (x-mu)**2 / (sigma + eps) ).sum(axis=1).mean(axis=0)
+        loss_sigma = torch.maximum(torch.tensor(0), torch.log(sogma.mean(axis=1) + eps)).mean(axis=0)
+        loss = loss_mu + loss_sigma
+        return loss
+
+    def loss_function(self, recon_x, x):
+
+        MSE = F.mse_loss(
+            recon_x.reshape(x.shape[0], -1), x.reshape(x.shape[0], -1), reduction="none"
+        ).sum(dim=-1)
+        return MSE.mean(dim=0)
+
+    @classmethod
+    def _load_model_config_from_folder(cls, dir_path):
+        file_list = os.listdir(dir_path)
+
+        if "model_config.json" not in file_list:
+            raise FileNotFoundError(
+                f"Missing model config file ('model_config.json') in"
+                f"{dir_path}... Cannot perform model building."
+            )
+
+        path_to_model_config = os.path.join(dir_path, "model_config.json")
+        model_config = AEConfig.from_json_file(path_to_model_config)
+
+        return model_config
+
 class AE(BaseAE):
     """Vanilla Autoencoder model.
 
@@ -301,133 +572,6 @@ class AE_Z_alpha2(BaseAE):
         if is_x_alpha:
             loss += self.loss_function(z_alpha,z_eps) #exp(-self.loss_function(z_alpha,z_alpha*0.)) + 0.1*
             #loss = self.loss_function(z_alpha,z_eps)
-
-        output = ModelOutput(loss=loss, recon_x=recon_x, z=z)
-
-        return output
-
-    def loss_function(self, recon_x, x):
-
-        MSE = F.mse_loss(
-            recon_x.reshape(x.shape[0], -1), x.reshape(x.shape[0], -1), reduction="none"
-        ).sum(dim=-1)
-        return MSE.mean(dim=0)
-
-    @classmethod
-    def _load_model_config_from_folder(cls, dir_path):
-        file_list = os.listdir(dir_path)
-
-        if "model_config.json" not in file_list:
-            raise FileNotFoundError(
-                f"Missing model config file ('model_config.json') in"
-                f"{dir_path}... Cannot perform model building."
-            )
-
-        path_to_model_config = os.path.join(dir_path, "model_config.json")
-        model_config = AEConfig.from_json_file(path_to_model_config)
-
-        return model_config
-
-class AE_multi_U(BaseAE):
-    """Vanilla Autoencoder model.
-
-    Args:
-        model_config (AEConfig): The Autoencoder configuration setting the main parameters of the
-            model.
-
-        encoder (BaseEncoder): An instance of BaseEncoder (inheriting from `torch.nn.Module` which
-            plays the role of encoder. This argument allows you to use your own neural networks
-            architectures if desired. If None is provided, a simple Multi Layer Preception
-            (https://en.wikipedia.org/wiki/Multilayer_perceptron) is used. Default: None.
-
-        decoder (BaseDecoder): An instance of BaseDecoder (inheriting from `torch.nn.Module` which
-            plays the role of decoder. This argument allows you to use your own neural networks
-            architectures if desired. If None is provided, a simple Multi Layer Preception
-            (https://en.wikipedia.org/wiki/Multilayer_perceptron) is used. Default: None.
-
-    .. note::
-        For high dimensional data we advice you to provide you own network architectures. With the
-        provided MLP you may end up with a ``MemoryError``.
-    """
-
-    def __init__(
-        self,
-        model_config: AEConfig,
-        encoder: Optional[BaseEncoder] = None,
-        decoder: Optional[BaseDecoder] = None,
-    ):
-
-        BaseAE.__init__(self, model_config=model_config, decoder=decoder)
-
-        self.model_name = "AE_multi_U"
-
-        if encoder is None:
-            if model_config.input_dim is None:
-                raise AttributeError(
-                    "No input dimension provided !"
-                    "'input_dim' parameter of BaseAEConfig instance must be set to 'data_shape' where "
-                    "the shape of the data is (C, H, W ..). Unable to build encoder "
-                    "automatically"
-                )
-
-            encoder = Encoder_AE_MLP(model_config)
-            self.model_config.uses_default_encoder = True
-
-        else:
-            self.model_config.uses_default_encoder = False
-
-        self.set_encoder(encoder)
-        self.p = 0 #p for the bernoulli encoding the number of missing values
-        self.nU = 100
-
-    def forward(self, inputs: BaseDataset, **kwargs) -> ModelOutput:
-        """The input data is encoded and decoded
-
-        Args:
-            inputs (BaseDataset): An instance of pythae's datasets
-
-        Returns:
-            ModelOutput: An instance of ModelOutput containing all the relevant parameters
-            - nU is the number of time we wish to makes copies of x and apply a different U
-            When self.p>0, we repeat nU times x and apply a different u everytime
-
-        """
-
-        x = inputs["data"]
-        u = (x!=-10)
-        nU = self.nU if self.training else 3
-        z = self.encoder(x).embedding 
-
-        if self.p>0:
-            x_repeat, z_repeat, u_repeat = x.repeat((nU,1)), z.repeat((nU,1)), u.repeat((nU,1)) #repeat nU times
-            #set xU as nU repeats of x with each time a different U applied
-            xU = x_repeat.detach().clone() 
-            U = tensor(binomial(n=1,p=self.p,size=xU.shape))
-            xU[U==1] = -10
-
-            zU = self.encoder(xU).embedding #encoding of xU
-
-        #if SNDS-like data, get z_alpha the equivalent of zU for SNDS-like data
-        is_x_alpha = (inputs['labels']==1.).float().mean()!=1.
-        if is_x_alpha:
-            x_alpha = inputs["labels"]
-            z_alpha = self.encoder.encoder_alpha(x_alpha)#.embedding
-            z_alpha = z_alpha.repeat((nU,1))
-
-        #Concatenate zU and z_alpha to get information from both for Z
-        if self.is_merging_net:
-            z = cat((zU,z_alpha),axis=1)
-            z = self.encoder.merging_net(z)
-        else:
-            z = zU
-
-        recon_x = self.decoder(z)["reconstruction"]
-        loss = self.loss_function(recon_x[u_repeat], x_repeat[u_repeat])
-        
-        if is_x_alpha:
-            loss += self.loss_function(z_alpha,zU) 
-        else:
-            loss += self.loss_function(z_repeat,zU) # + exp(-self.loss_function(z,z*0.))
 
         output = ModelOutput(loss=loss, recon_x=recon_x, z=z)
 
--- src/pythae/models/nn/__init__.py
+++ src/pythae/models/nn/__init__.py
@@ -18,6 +18,32 @@ from typing import List
 import torch.nn as nn
 from torch import cat, stack
 
+class Encoder_vAE(BaseEncoder): #corresponds to Encoder_AE_w_variance on pdds platform code but with no Z_alpha
+    def __init__(self, args):
+        BaseEncoder.__init__(self)
+        self.input_dim = args.input_dim[0]
+        self.latent_dim = args.latent_dim
+
+        self.conv_layers = nn.Sequential(
+            nn.Linear(self.input_dim * 2, self.input_dim*10),
+            nn.ReLU(),
+            nn.Linear(self.input_dim*10, 1024),
+            nn.ReLU(),
+        )
+
+        self.embedding = nn.Linear(1024, args.latent_dim)
+        self.log_var = nn.Sequential(nn.Linear(1024, args.latent_dim),nn.ReLU())
+
+    def forward(self, x: torch.Tensor):
+        u = (x!=-10)
+        xU = cat((x,u),axis=1)
+        h1 = self.conv_layers(xU).reshape(x.shape[0], -1)
+        output = ModelOutput(
+            embedding=self.embedding(h1),
+            log_covariance=self.log_var(h1)
+        )
+        return output
+
 class Encoder_VAE_missing(BaseEncoder):
     def __init__(self, args):
         BaseEncoder.__init__(self)
