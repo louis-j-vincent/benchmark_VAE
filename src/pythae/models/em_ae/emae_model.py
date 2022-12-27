1596b733331f8d1cf6dfbd33cf9e13d8121fc1d3
Louis VINCENT <louisjvincent@gmail.com>
2022-12-27 16:50:25 +0100

    detach

--- src/pythae/models/em_ae/emae_model.py
+++ src/pythae/models/em_ae/emae_model.py
@@ -185,16 +185,16 @@ class EMAE(AE):
         for i in range(1):
 
             #E-step
-            tau = self.labels[:,:self.K].detach().cpu()
+            tau = self.labels[:,:self.K]#.detach().cpu()
             #print(tau.mean())
             ## add this to complete missing values
-            missing_labels = torch.where(self.labels[:,-1]==1)[0].detach().cpu()
+            missing_labels = torch.where(self.labels[:,-1]==1)[0]#.detach().cpu()
             Y = (Z[:,None,:]-self.mu[None,:,:]) #shape: n_obs, k_means, d_dims
             Sigma = self.Sigma[None,:,:] 
             N_log_prob = -0.5* ( Y**2/Sigma + torch.log(2*torch.pi*Sigma) )#.sum(axis=0)
             log_tau = torch.log(self.alpha+1e-5)+N_log_prob.sum(axis=2)
-            log_tau = (log_tau - torch.logsumexp(log_tau, axis=1)[:,None]).detach().cpu()
-            tau[missing_labels] = torch.exp(log_tau[missing_labels]).detach().cpu() #p(x_i ; z_i = k) p(z_i = k)
+            log_tau = (log_tau - torch.logsumexp(log_tau, axis=1)[:,None])#.detach().cpu()
+            tau[missing_labels] = torch.exp(log_tau[missing_labels])#.detach().cpu() #p(x_i ; z_i = k) p(z_i = k)
             #print(tau.mean())
 
             # M-step
