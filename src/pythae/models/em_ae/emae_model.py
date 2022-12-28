56309aaf1fe1342c0e17b5f947f69b45d53225ab
Louis VINCENT <louisjvincent@gmail.com>
2022-12-28 18:25:04 +0100

    debug cheat

--- src/pythae/models/em_ae/emae_model.py
+++ src/pythae/models/em_ae/emae_model.py
@@ -361,6 +361,8 @@ class EMAE(AE):
                 print('delta estim: ',delta_estim.mean().item())
                 print(delta_estim.mean(axis=1))
                 print(delta_estim.mean(axis=0))
+                tau[missing_labels] = self.labels[missing_labels,self.K:]
+                print('Now im cheating by adding the right labels')
 
                 #plt.scatter(torch.exp(log_tau[~missing_labels]).detach().cpu(),tau[~missing_labels].detach().cpu());
                 #plt.show();
