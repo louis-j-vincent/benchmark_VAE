c1ff81e7aef9bc0f03274057af1940f547ab4422
Louis VINCENT <louisjvincent@gmail.com>
2023-01-09 16:20:31 +0100

    add missing_ae folder

---
+++ src/pythae/models/missing_ae/__init__.py
@@ -0,0 +1,18 @@
+"""Implementation of a Vanilla Autoencoder model.
+
+Available samplers
+-------------------
+
+.. autosummary::
+    ~pythae.samplers.NormalSampler
+    ~pythae.samplers.GaussianMixtureSampler
+    ~pythae.samplers.MAFSampler
+    ~pythae.samplers.IAFSampler
+    :nosignatures:
+
+"""
+
+from .ae_config import AEConfig
+from .ae_model import AE, AE_Z_alpha, AE_Z_alpha2, AE_multi_U, vAE
+
+__all__ = ["AE","AE_Z_alpha","AE_Z_alpha2","AE_multi_U","vAE","AEConfig"]
