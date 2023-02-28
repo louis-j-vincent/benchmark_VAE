24383f06128aac3bb683f5526fbecabc69b15005
Louis Vincent <louis.vincent@implicity.fr>
2023-02-28 19:32:22 +0100

    Fix error in param update of tau : missing labels

--- src/pythae/models/vfemae/vfemae_model.py
+++ src/pythae/models/vfemae/vfemae_model.py
@@ -85,10 +85,12 @@ class vfEMAE(BaseAE): #equivalent of AE_multi_U_w_variance
         self.tempered_EM = False
         self.deterministic_EM = True
         self.print = False
+        self.Z = torch.tensor([])
+        self.labels = torch.tensor([])
+        self.tempo = torch.tensor([])
 
         ##init EMAE params
         self.init = True
-        self.Z, self.labels = None, None
         self.K = model_config.K #nb of Gaussians
         device = "cuda" if torch.cuda.is_available() else "cpu"
         self.mu = torch.zeros((self.K,model_config.latent_dim)).to(device)
@@ -131,71 +133,73 @@ class vfEMAE(BaseAE): #equivalent of AE_multi_U_w_variance
         y_missing = F.one_hot(inputs["labels"].to(torch.int64),num_classes=self.K*3).float().to(self.device) #3 to overshoot if there are many classes
         y = y_missing[:,:self.K]
 
-        if self.variationnal: #do a VAE-like step by sampling on a gaussian centered on z and "contained" inside the class gaussian
-            ratio = ((self.quantile)/(1.96 + torch.abs(z - y@self.mu)))**2
-            sigma_small_max = torch.fmax((y@self.Sigma**0.5)*ratio,torch.tensor(0.001)) 
-            z_var = z + torch.normal(torch.zeros(z.shape).to(self.device),sigma_small_max).to(self.device)
-        else:
-            z_var = z
+        LLloss, classif_loss = self.likelihood_loss(z,y_missing) #Log likelihood loss on all data 
+        
+        ## one step of EMAE variationnal sampling
+        z_var = self.variationnal_sampling(y,z)
 
         ## NORMAL vAE step
-
-        #if self.p>0: #augment data
         X, Z = x.repeat_interleave(n_repeats,dim=0), z.repeat_interleave(n_repeats,dim=0)
         XV = self.corrupt(X) #corrupt by adding missing values
-        ZV = self.encoder(XV) #encoding of xU
-        try:
-            ZV_mu, ZV_sigma = ZV.embedding, torch.exp(ZV.log_covariance)
-        except AttributeError:
-            ZV_mu = ZV.embedding
 
-        XV_hat = self.decoder(ZV_mu)["reconstruction"]
-        if XV_hat.shape != X.shape:
-            XV_hat = torch.squeeze(XV_hat, 1) #if dimension added
+        ZV_mu = self.encoder(XV).embedding #encoding of xU
+        XV_hat = self.decoder(ZV_mu)["reconstruction"].reshape(ZV_mu.shape[0],x.shape[-1])
+        #if XV_hat.shape != X.shape:
+        #    XV_hat = torch.squeeze(XV_hat, 1) #if dimension added
+
         recon_loss = self.loss_function(XV_hat[(X!=-10)], X[(X!=-10)])
+        temporal_loss = self.temporal_loss(ZV_mu,X,tempo,n_repeats)
 
-        ## EMAE step
-        #print(z[y_missing[:,0]==1].shape)
-        #if (y_missing[:,0]==1).float().mean()!=0 and (y_missing[:,0]==1).float().mean()!=1:
-        #    recon_loss += self.gamma/(self.loss_function(z[y_missing[:,0]==1].mean(axis=0).reshape(1,-1), z[y_missing[:,0]==0].mean(axis=0).reshape(1,-1)))
-
-
-        if self.Z is None: #this will be true at the beginning of each epoch
-            self.Z = z
-            self.labels = y_missing
-            self.tempo = tempo
-        else:
-            self.Z = torch.cat((self.Z, z),0)
-            self.labels = torch.cat((self.labels, y_missing),0)
-            self.tempo = torch.cat((self.tempo, tempo),0)
-
-        delta_Z = torch.abs(ZV_mu[1:] - ZV_mu[:-1])
-        delta_X = torch.abs(X[1:] - X[:-1])**2 + 0.0001
-        U = (X==-10)
-        delta_U = (1 - U[1:]*U[:-1]*1) + 0.0001
-        temporal_loss = torch.abs( delta_Z.mean(axis=-1) * delta_U.sum(axis=-1) / (delta_X * delta_U).sum(axis=-1) ).mean() * (delta_X * delta_U).mean()
-        # we divide (aka multiply here) by delta_U sum to get mean of delta_x * delta_U only where delta_U==1
-        #print(temporal_loss)
-
-        #if self.beta>0 and self.temperature > self.temp_start: 
-
-        LLloss, classif_loss = self.likelihood_loss(z,y_missing) #Log likelihood loss on all data 
-        #LLloss_true = self.likelihood_loss(z,y_missing,ll_with_missing_labels=False) #Log likelihood only on labelised data
-
-        #LLloss_tempered = (LLloss*self.temperature  + (1-self.temperature)*LLloss_true)*self.temperature
-        LLloss_tempered = LLloss + classif_loss
-
+        loss = recon_loss + temporal_loss * self.gamma
         if self.temperature > self.temp_start:
-            loss = recon_loss + LLloss_tempered*self.beta*self.temperature
-        else:
-            loss = recon_loss
-        #print to observe evolution of different losses
-        loss += temporal_loss * self.gamma
+            loss += (LLloss + classif_loss)*self.beta*self.temperature 
+
+        ## update args
+        self.Z = torch.cat((self.Z, z),0)
+        self.labels = torch.cat((self.labels, y_missing),0)
+        self.tempo = torch.cat((self.tempo, tempo),0)
+        self.centroids.append(z.detach().to(self.device))
+        self.M.append( (torch.eye(z.shape[-1]).repeat(z.shape[0],1,1) * tempo[:,None,None]).to(self.device) ) #id matrices whose size increases with time to hosp
+
+        ## print to observe evolution of different losses
         if self.print:
-           print(f'Losses: e{self.epoch:.4f} - recon {recon_loss.item():.4f} - LLoss_total {LLloss.item():.4f} classif_loss {classif_loss.item():.4f} total_loss {loss.item():.4f}')
+           print(f'Losses: e{self.epoch:.4f} - recon {recon_loss.item():.4f} - LLoss_total {LLloss.item():.4f} classif_loss {classif_loss.item():.4f} temporal_loss {temporal_loss.item():.4f} total_loss {loss.item():.4f}')
+        
         output = ModelOutput(loss=recon_loss + self.beta*(LLloss + classif_loss) + temporal_loss*self.gamma, recon_x=XV_hat, z=Z)
 
         return output
+
+    def variationnal_sampling(self,y,z):
+        if self.variationnal: #do a VAE-like step by sampling on a gaussian centered on z and "contained" inside the class gaussian
+            ratio = ((self.quantile)/(1.96 + torch.abs(z - y@self.mu)))**2
+            sigma_small_max = torch.fmax((y@self.Sigma**0.5)*ratio,torch.tensor(0.001)) 
+            return z + torch.normal(torch.zeros(z.shape).to(self.device),sigma_small_max).to(self.device)
+        else:
+            return z
+
+    def temporal_loss(self,Z,X,tempo,n_repeats):
+        """
+        Temporal loss
+
+        # we divide (aka multiply here) by delta_U sum to get mean of delta_x * delta_U only where delta_U==1
+            #print(temporal_loss)
+        """
+        delta_Z = torch.abs(Z[1:] - Z[:-1])
+        delta_X = torch.abs(X[1:] - X[:-1])**2 + 0.0001
+        U = (X==-10)
+        delta_U = (1 - U[1:]*U[:-1]*1) + 0.0001
+        return torch.abs( delta_Z.mean(axis=-1) * delta_U.sum(axis=-1) / (delta_X * delta_U).sum(axis=-1) ).mean() * (delta_X * delta_U).mean()
+        
+    def temporal_loss(self,Z,X,tempo,n_repeats):
+        """
+        Temporal loss
+
+        # we divide (aka multiply here) by delta_U sum to get mean of delta_x * delta_U only where delta_U==1
+            #print(temporal_loss)
+        """
+        delta_Z = torch.abs(Z[1:] - Z[:-1])
+        TEMPO = tempo.repeat_interleave(n_repeats,dim=0)
+        return torch.abs(delta_Z[TEMPO[:-1]!=1]).mean()
 
     def corrupt(self, X):
         """
@@ -214,12 +218,62 @@ class vfEMAE(BaseAE): #equivalent of AE_multi_U_w_variance
 
     def loss_function(self, recon_x, x):
 
+        if recon_x.shape != x.shape:
+            recon_x = torch.squeeze(recon_x, 1) #if dimension added
+
         MSE = F.mse_loss(
             recon_x.reshape(x.shape[0], -1), x.reshape(x.shape[0], -1), reduction="none"
         ).sum(dim=-1)
         return MSE.mean(dim=0)
 
 ## compute likelihood loss
+
+    def E_step(self, Z):
+        """
+        Given Z, computes and returns tau the matrix of probabilities of belonging to each gaussian
+        """
+
+        Y = (Z[:,None,:]-self.mu[None,:,:]) #shape: n_obs, k_means, d_dims
+        Sigma = self.Sigma[None,:,:] + 0.0001
+
+        #E-step
+        N_log_prob = -0.5* ( Y**2/Sigma + torch.log(2*np.pi*Sigma) )
+        #print('****')
+        #print(N_log_prob.sum(axis=2).sum(axis=1))
+        log_tau = torch.log(self.alpha[None,:] + 1e-4) + N_log_prob.sum(axis=2) #log [ p(x_i ; z_i = k) p(z_i = k)]
+        log_tau = (log_tau - torch.logsumexp(log_tau, axis=1)[:,None])#.detach().cpu()
+        self.log_tau = log_tau
+        tau = torch.exp(log_tau)
+        for i in range(tau.shape[0]):
+            if tau[i].sum()==0:
+                print('')
+                print(i,tau[i],log_tau[i])
+                print(stop)
+        #tau_ = torch.exp(log_tau)
+        #tau = tau_ / tau_.sum(axis=1)[:,None]
+        return tau , Y, Sigma
+
+    def log_prob(self,Y,Sigma,tau):
+        """
+        Given Z and tau, returns the log prob
+        """
+        N_log_prob = torch.minimum(-0.5* ( Y**2/Sigma + torch.log(2*np.pi*Sigma)),torch.tensor(0) )#.sum(axis=0)
+        N_prob = (torch.exp(N_log_prob) * tau[:,:self.K,None]).sum(axis=1) #only use the kth gaussian 
+        prob = torch.mean(N_prob[N_prob==N_prob]) #nanmean walkaround
+
+        #if tau_prior is not None:
+        #    prob -= ((tau.mean(axis=0) - self.alpha)**2 / 2 ).sum()
+
+        return prob
+
+    def M_step(self, tau, Z, mu_prior=None):
+
+        tau_sum = tau[:,:,None].sum(axis=0).detach().cpu()
+        mu = tau.T@Z.detach().cpu() / tau_sum
+        Sigma = (tau[:,:,None] * (Z[:,None,:].detach().cpu()-mu[None,:,:].detach().cpu())**2).sum(axis=0).detach().cpu()/tau_sum + 0.00001
+        mu[self.i_i50] = 0
+        Sigma[self.i_i50] = 0.001
+        return mu, Sigma
 
     def likelihood_loss(self, Z, y, ll_with_missing_labels = True):
         """
@@ -228,56 +282,18 @@ class vfEMAE(BaseAE): #equivalent of AE_multi_U_w_variance
         - computing the likelihood loss defined as sum_i sum_k tau_i,k * P(z_i sachant mu_k, Sigma_k)
         """
 
-        tau = torch.clone(y[:,:self.K])#.detach().cpu()
         missing_labels = (y[:,self.K:].sum(axis=1)>0)
 
         #E-step
-        Y = (Z[:,None,:]-self.mu[None,:,:]) #shape: n_obs, k_means, d_dims
-        Sigma = self.Sigma[None,:,:] + 0.0001
-        N_log_prob = -0.5* ( Y**2/Sigma + torch.log(2*np.pi*Sigma) )
-        log_tau = torch.log(self.alpha+1e-5)+N_log_prob.sum(axis=2) #log [ p(x_i ; z_i = k) p(z_i = k)]
-        log_tau = (log_tau - torch.logsumexp(log_tau, axis=1)[:,None])#.detach().cpu()
-
-        loss_classif = F.l1_loss( tau[~missing_labels], torch.exp(log_tau[~missing_labels]) ) * self.K
-
-        tau = torch.exp(log_tau)
+        tau, Y, Sigma = self.E_step(Z)
 
         #get log prob
-        N_log_prob = torch.minimum(-0.5* ( Y**2/Sigma + torch.log(2*np.pi*Sigma)),torch.tensor(0) )#.sum(axis=0)
-        N_prob = (torch.exp(N_log_prob) * tau[:,:self.K,None]).sum(axis=1) #only use the kth gaussian 
-        prob = torch.mean(N_prob[N_prob==N_prob]) #nanmean walkaround
+        prob = self.log_prob(Y,Sigma,tau)
 
-        pk_prior = ((tau[missing_labels].mean(axis=0) - y[:,:self.K].mean(axis=0))**2 / 2 ).sum()
-
-        prob = prob - pk_prior
+        tau_prior = torch.clone(y[:,:self.K])#.detach().cpu()
+        loss_classif = F.l1_loss( tau_prior[~missing_labels], tau[~missing_labels] )
 
         return 1 - prob.mean(), loss_classif
-
-    def tau_loss(self,z,y):
-        tau = self.compute_tau(z,y,replace_y=True)
-        loss = F.l1_loss(tau,y)
-        return loss
-
-    def compute_tau(self, Z, y, replace_y = False):
-        """
-        Compute probability for each point to be a aprt of each gaussian
-        """
-
-        tau = torch.clone(y[:,:self.K])#.detach().cpu()
-        missing_labels = torch.where(y[:,self.K:].sum(axis=1)>0)[0]#.detach().cpu()
-
-        #E-step
-        Y = (Z[:,None,:]-self.mu[None,:,:]) #shape: n_obs, k_means, d_dims
-        Sigma = self.Sigma[None,:,:] 
-        N_log_prob = -0.5* ( Y**2/Sigma + torch.log(2*np.pi*Sigma) )#.detach().cpu()
-        log_tau = torch.log(self.alpha+1e-5)+N_log_prob.sum(axis=2) #log [ p(x_i ; z_i = k) p(z_i = k)]
-        log_tau = (log_tau - torch.logsumexp(log_tau, axis=1)[:,None])#.detach().cpu()
-        if replace_y:
-            tau = torch.exp(log_tau)#
-        else:
-            tau[missing_labels] = torch.exp(log_tau[missing_labels])#
-
-        return tau
 
 ## update parameters
 
@@ -285,58 +301,35 @@ class vfEMAE(BaseAE): #equivalent of AE_multi_U_w_variance
         """
         Run self.EM_steps (set to 1) steps of the EM algorithm to estimate parameters 
         """
-        labels = self.labels[:,:self.K]
 
-        if Z is None:
+        labels = self.labels[:,:self.K]
+        tau = torch.clone(labels).detach().cpu()
+        missing_labels = (labels.sum(axis=1)==0)
+
+        if Z==[] or Z is None:
             Z = self.Z
         if self.init==True:
             self.mu = torch.matmul(labels.T,Z)
-            self.alpha = torch.mean(labels[labels==labels],axis=0) + (1/self.K)*(self.labels[:,self.K:].sum(axis=1).mean())
+            self.alpha = labels.sum(axis=0) / labels.sum()
             self.init=False
 
         print('Updating parameters')
         for i in range(self.EM_steps):
-            #E-step
-            tau = torch.clone(labels).detach().cpu()
-            #infer missing labels
-            missing_labels = torch.where(self.labels[:,self.K:].sum(axis=1)>0)[0].detach().cpu()
-            Y = (Z[:,None,:]-self.mu[None,:,:]) #shape: n_obs, k_means, d_dims
-            Sigma = self.Sigma[None,:,:]  + 0.0001
-            N_log_prob = -0.5* ( Y**2/Sigma + torch.log(2*np.pi*Sigma) )#.detach().cpu()
-            log_tau = torch.log(self.alpha+1e-5)+N_log_prob.sum(axis=2) #log [ p(x_i ; z_i = k) p(z_i = k)]
-            log_tau = (log_tau - torch.logsumexp(log_tau, axis=1)[:,None]).detach().cpu()
-            tau[missing_labels] = torch.exp(log_tau[missing_labels]) 
-            #valid_keys = (log_tau[missing_labels] == log_tau[missing_labels]) #don't replace nan values
-            #tau[missing_labels][valid_keys] = torch.exp(log_tau[missing_labels][valid_keys]) 
-            tau[tau!=tau] = 0
 
-            tau = tau.detach().cpu()
+            #E-step
+            tau_new, Y, Sigma = self.E_step(Z)
+            tau_new = tau_new.detach().cpu()
+            tau[missing_labels] = tau_new[missing_labels]
+            for i in range(tau.shape[0]):
+                if tau[i].sum()==0:
+                    print('')
+                    print(i,tau[i],tau_new[i])
+                    #print(stop_new)
 
             #M-step
-            if self.deterministic_EM: #EM only on observed variables
-                print('M step only on deterministic labels')
-                mu, Sigma = self.M_step(tau[~missing_labels], Z[~missing_labels], self.mu)
-                self.mu[mu==mu] = mu[mu==mu] #replace when different from nan
-                self.Sigma[Sigma==Sigma] = Sigma[Sigma==Sigma] #replace when different from nan
-
-            elif self.tempered_EM:
-
-                mu_0, Sigma_0 = self.M_step(tau[missing_labels], Z[missing_labels])
-                mu_1, Sigma_1 = self.M_step(tau[~missing_labels], Z[~missing_labels])
-
-                missing_ratio = len(missing_labels)/len(tau)
-                t0, t1 = self.temperature*(1-missing_ratio), (1 - self.temperature)*missing_ratio
-
-                mu = torch.mean(torch.stack((mu_0*t0,mu_1*t1),axis=2),axis=2)
-                Sigma = torch.mean(torch.stack((Sigma_0*t0,Sigma_1*t1),axis=2),axis=2)
-
-                self.mu[mu==mu] = mu[mu==mu] #replace when different from nan
-                self.Sigma[Sigma==Sigma] = Sigma[Sigma==Sigma] #replace when different from nan
-
-            else:
-                mu, Sigma = self.M_step(tau, Z)
-                self.mu[mu==mu] = mu[mu==mu] #replace when different from nan
-                self.Sigma[Sigma==Sigma] = Sigma[Sigma==Sigma] #replace when different from nan
+            self.mu, self.Sigma = self.M_step(tau[~missing_labels], Z[~missing_labels], self.mu)
+            if (self.mu!=self.mu).sum()>0:
+                print(stopping)
 
             #set to device
             self.tau = tau.to(self.device)
@@ -364,7 +357,8 @@ class vfEMAE(BaseAE): #equivalent of AE_multi_U_w_variance
             for i in range(self.K):
                 mu = (self.mu[i]@pca.components_.T).detach().numpy()
                 Sigma = (pca.components_@torch.diag(self.Sigma[i]).detach().numpy()@pca.components_.T)
-                ax[0] = self.draw_95_ellipse(mu, Sigma, alpha = tau_sum[i], ax=ax[0])
+                color = 'r' if i==self.i_i50 else 'k'
+                ax[0] = self.draw_95_ellipse(mu, Sigma, alpha = tau_sum[i], ax=ax[0], c=color)
 
             step = [0] + list(np.where(self.tempo.detach().numpy()==1)[0])
             for i in range(30):
@@ -375,7 +369,7 @@ class vfEMAE(BaseAE): #equivalent of AE_multi_U_w_variance
                         y = y/max(y)
 
                     ax[1].plot(X[step[i]:step[i+1],0],X[step[i]:step[i+1],1],alpha=0.3)
-                    ax[1].scatter(X[step[i]:step[i+1],0],X[step[i]:step[i+1],1],c=y*50,alpha=0.8)
+                    ax[1].scatter(X[step[i]:step[i+1],0],X[step[i]:step[i+1],1],c=y,alpha=0.8)
 
             img = tau[::1][:200].detach().numpy().T
             img[0] = (self.tempo==1)[:200]
@@ -385,19 +379,13 @@ class vfEMAE(BaseAE): #equivalent of AE_multi_U_w_variance
             fig.savefig(f'plot{self.epoch:03d}.png')
         
         self.set_vector_field()
-        self.Z = None
+        self.Z = torch.tensor([])
+        self.labels = torch.tensor([])
+        self.tempo = torch.tensor([])
+
         self.hist['mu'] = torch.vstack([self.hist['mu'],self.mu])
         self.hist['Sigma'] = torch.vstack([self.hist['Sigma'],self.Sigma])
         self.hist['beta'] += [self.beta]
-
-    def M_step(self, tau, Z, mu_prior=None):
-
-        tau_sum = tau[:,:,None].sum(axis=0).detach().cpu()
-        #mu = (tau[:,:,None]*Z[:,None,:].detach().cpu()).sum(axis=0).detach().cpu()/tau_sum
-        mu = tau.T@Z.detach().cpu() / tau.sum(axis=0).reshape(-1,1).repeat(1,Z.shape[-1]).detach().cpu()
-        Sigma = (tau[:,:,None] * (Z[:,None,:].detach().cpu()-mu[None,:,:].detach().cpu())**2).sum(axis=0).detach().cpu()/tau_sum + 0.00001
-        #mu, Sigma = self.mu, self.Sigma
-        return mu, Sigma
 
 ## compute vector field
 
@@ -429,7 +417,38 @@ class vfEMAE(BaseAE): #equivalent of AE_multi_U_w_variance
 
     def compute_vector(self,z):
         v = self.vector_kernel_dist(z,self.v_start) @ self.v / self.vector_kernel_dist(z,self.v_start).sum(axis=1)[:,None]
-        return v
+        return v, self.vector_kernel_dist(z,self.v_start)
+
+## compute Riemannian metric
+
+    def G_inv(self,z):
+
+        self.T = 1 #added
+        self.lbd = 0
+
+        # convert to 1 big tensor
+        self.M_tens = torch.cat(self.M)
+        self.centroids_tens = torch.cat(self.centroids)
+
+        #print(self.centroids_tens.unsqueeze(0), z.unsqueeze(1))
+        
+        return (
+            self.M_tens.unsqueeze(0)
+            * torch.exp(
+                -torch.norm(
+                    self.centroids_tens.unsqueeze(0) - z.unsqueeze(1), dim=-1
+                )
+                ** 2
+                / (self.T ** 2)
+            )
+            .unsqueeze(-1)
+            .unsqueeze(-1)
+        ).sum(dim=1) + self.lbd * torch.eye(self.latent_dim).to(self.device)
+
+        #self.G = G
+        self.G_inv = G_inv
+        self.M = []
+        self.centroids = []
 
 ## plot intermediate EM steps
 
